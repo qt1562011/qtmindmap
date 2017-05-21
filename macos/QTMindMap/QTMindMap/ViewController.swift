df44bbae7e04c6762f024163c6c62ff9dd0e897e
Moca <lam.phan@moca.vn>
2017-05-21 10:33:24 +0700

    create masos project

---
+++ macos/QTMindMap/QTMindMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QTMindMap
+//
+//  Created by Moca on 5/21/17.
+//  Copyright © 2017 QT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
